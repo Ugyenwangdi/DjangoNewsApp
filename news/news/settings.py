9489a2cb6c6697de5ab7fd4fa7b011c9719c2e14
wulfi code <wulficode@gmail.com>
2022-01-20 18:30:00 +0600

    first commit

--- news/news/settings.py
+++ news/news/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'news.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
+    # sql database
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    # postgresql database
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': '',
+       'USER': '',
+       'PASSWORD': 'uGENDI^000',
+       'HOST': '',
+       'PORT': '',
+   }
 }
 
 
